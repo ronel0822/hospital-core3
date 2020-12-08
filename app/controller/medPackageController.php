7119cc55c65b432f35fc46188f8acc15fd15445c
ronel0822 <ronelocampo0822@gmail.com>
2020-12-08 20:52:48 +0800

    This is the final Count down

--- app/controller/medPackageController.php
+++ app/controller/medPackageController.php
@@ -9,25 +9,4 @@ class medPackageController extends Controller{
 		$this->view->render();
 	}
 
-	public function medPackageView(){
-		$this->model('medPackage');
-		$this->view('medPackage\medPackageView',['mainData'=>$this->model->getAllMedPackage()]);
-		$this->view->page_title = 'Medical Package';
-		$this->view->render();
-	}
-
-	public function medPackageAvailList(){
-		$this->model('medPackage');
-		$this->view('medPackage\medPackageAvailList',['availData'=>$this->model->getAllMedPackageAvail()]);
-		$this->view->page_title = 'Medical Package';
-		$this->view->render();
-	}
-
-	public function medCreatePackage(){
-		$this->model('medPackage');
-		$this->view('medPackage\medCreatePackage');
-		$this->view->page_title = 'Medical Package';
-		$this->view->render();
-	}
-	
 }
