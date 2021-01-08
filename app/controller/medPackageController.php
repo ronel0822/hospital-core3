2aab2b6049e4fd08f1645c1a39a73bf1c265823f
Ronstadt Tabianan <75000821+RT-10@users.noreply.github.com>
2021-01-09 03:44:26 +0800

    save
    save

--- app/controller/medPackageController.php
+++ app/controller/medPackageController.php
@@ -4,33 +4,51 @@ class medPackageController extends Controller{
 
 	public function medPackageList(){
 		$this->model('medPackage');
-		$this->view('medPackage\medPackageList',['mainData'=>$this->model->getAllMedPackage()]);
+		$this->view('medPackage'.DIRECTORY_SEPARATOR.'medPackageList',['mainData'=>$this->model->getAllMedPackage()]);
 		$this->view->page_title = 'Medical Package';
 		$this->view->render();
 	}
 
 	public function medPackageView($id){
 		$this->model('medPackage');
+<<<<<<< Updated upstream
 		$this->view('medPackage\medPackageView',[
 			'medPackageInfo'=>$this->model->getPackageListInfo($id)
 		]);
 		$this->view->page_title = 'Drugs';
+=======
+		$this->view('medPackage'.DIRECTORY_SEPARATOR.'medPackageView',[
+			'medPackageInfo'=>$this->model->getPackageListInfo($id)
+		]);
+		$this->view->page_title = 'Medical Package';
+>>>>>>> Stashed changes
 		$this->view->render();
 	}
 
 	public function medPackageAvailList(){
 		$this->model('medPackage');
+<<<<<<< Updated upstream
 		$this->view('medPackage\medPackageAvailList',['availData'=>$this->model->getAllAvailPackage()]);
+=======
+		$this->view('medPackage'.DIRECTORY_SEPARATOR.'medPackageAvailList',['availData'=>$this->model->getAllAvailPackage()]);
+>>>>>>> Stashed changes
 		$this->view->page_title = 'Medical Package';
 		$this->view->render();
 	}
 
 	public function medCreatePackage(){
 		$this->model('medPackage');
+<<<<<<< Updated upstream
 		$this->view('medPackage\medCreatePackage',['createPackage'=>$this->model->getAllAvailPackage()]);
 		$this->view->page_title = 'Medical Package';
 		$this->view->render();
 	}
 
 
+=======
+		$this->view('medPackage'.DIRECTORY_SEPARATOR.'medCreatePackage',['createPackage'=>$this->model->getAllAvailPackage()]);
+		$this->view->page_title = 'Medical Package';
+		$this->view->render();
+	}
+>>>>>>> Stashed changes
 }
--- app/model/medPackage.php
+++ app/model/medPackage.php
@@ -22,16 +22,28 @@ class medPackage extends Database{
 	}
 
 	function getPackageListInfo($id){
+<<<<<<< Updated upstream
 		$query = "SELECT * FROM core3_med_avail 
 				INNER JOIN core3_med_pack
 				ON core3_med_avail.med_pack_id = core3_med_pack.id
 				INNER JOIN core3_med_pack_inc 
 				ON core3_med_pack.id = core3_med_pack_inc.med_pack_id
 				WHERE core3_med_avail.id = ?";
+=======
+		$query = "SELECT * FROM CORE3_med_pack 
+				INNER JOIN core3_med_pack_inc 
+				ON CORE3_med_pack.id = core3_med_pack_inc.med_pack_id
+				WHERE core3_med_pack.id =?";
+>>>>>>> Stashed changes
 		$stmt = $this->connect()->prepare($query);
 		$stmt->bindParam(1,$id);
 		$stmt->execute();
 		return $stmt->fetchAll();			
 	}
 
+<<<<<<< Updated upstream
+=======
+	
+
+>>>>>>> Stashed changes
 }
