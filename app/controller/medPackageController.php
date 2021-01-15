5502603b9ea4085e64dd52267d45b6df268be353
Ronstadt Tabianan <75000821+RT-10@users.noreply.github.com>
2021-01-16 03:22:46 +0800

    add
    add create package

--- app/controller/medPackageController.php
+++ app/controller/medPackageController.php
@@ -4,9 +4,71 @@ class medPackageController extends Controller{
 
 	public function medPackageList(){
 		$this->model('medPackage');
-		$this->view('medPackage\medPackageList',['mainData'=>$this->model->getAllMedPackage()]);
+		$this->view('medPackage'.DIRECTORY_SEPARATOR.'medPackageList',[
+			'mainData'=>$this->model->getAllMedPackage()]);
 		$this->view->page_title = 'Medical Package';
 		$this->view->render();
 	}
+
+	public function medPackageView($id){
+		$this->model('medPackage');
+		$this->view('medPackage'.DIRECTORY_SEPARATOR.'medPackageView',[
+			'medPackageInfo'=>$this->model->getPackageListInfo($id)]);
+		$this->view->page_title = 'Medical Package';
+		$this->view->render();
+	}
+
+	public function medPackageAvailList(){
+		$this->model('medPackage');
+		$this->view('medPackage'.DIRECTORY_SEPARATOR.'medPackageAvailList',[
+			'availData'=>$this->model->getAllAvailPackage()]);
+		$this->view->page_title = 'Medical Package';
+		$this->view->render();
+	}
+
+	public function medPackageAvailView($id){
+		$this->model('medPackage');
+		$this->view('medPackage'.DIRECTORY_SEPARATOR.'medPackageAvailView',[
+			'medPackageAvailView'=>$this->model->viewAvailInfo($id)]);
+		$this->view->page_title = 'Medical Package';
+		$this->view->render();
+	}
+
+	public function medCreatePackage(){
+		$this->model('medPackage');
+		$this->view('medPackage'.DIRECTORY_SEPARATOR.'medCreatePackage',[
+			'createPackage'=>$this->model->getIdMedPackage()]);
+		$this->view->page_title = 'Medical Package';
+		$this->view->render();
+	}
+
+	public function createNewPackage(){
+		$this->model('medPackage');
+		if(isset($_POST['submit'])){
+			if($this->model->createNewPackage(
+					$_POST['id'],	
+					$_POST['packageName'],
+					$_POST['packagePrice'],
+					$_POST['packageDesc'])){
+				$class = "alert alert-success";
+				$message = "Package Created Successfully!";
+				$this->view('medPackage'.DIRECTORY_SEPARATOR.'medPackageList',[
+					'mainData'=>$this->model->getAllMedPackage(),
+					'class'=>$class,'message'=>$message]);
+				$this->view->page_title = 'Medical Package';
+				$this->view->render();
+			}
+
+		}else{
+			$class = "alert alert-warning";
+			$message = "No data found. <a href='/medPackage/medCreatePackage'>Click here.</a> to refresh.";
+			$this->view('medPackage'.DIRECTORY_SEPARATOR.'medCreatePackage',[
+				'medCreatePackage'=>$this->model->getAllAvailPackage(),
+				'class'=>$class,'message'=>$message]);
+			$this->view->page_title = 'Medical Package';
+			$this->view->render();
+		}
+	}
+
 
 }
--- app/model/medPackage.php
+++ app/model/medPackage.php
@@ -2,9 +2,71 @@
 
 class medPackage extends Database{
 
-	//Get all drugs
+	//Get all Medical Package
 	function getAllMedPackage(){
 		$query = "SELECT * FROM core3_med_pack ORDER BY id DESC";
+		$stmt = $this->connect()->prepare($query);
+		$stmt->execute();
+		return $stmt->fetchAll();
+	}
+
+	//Get all Avail Package
+	function getAllAvailPackage(){
+		$query = "SELECT * FROM core3_med_avail 
+				INNER JOIN core3_med_pack
+				ON core3_med_avail.med_pack_id = core3_med_pack.id";
+		$stmt = $this->connect()->prepare($query);
+		$stmt->execute();
+		return $stmt->fetchAll();
+	}
+
+	//View Package information
+	function getPackageListInfo($id){
+		$query = "SELECT * FROM CORE3_med_pack 
+				INNER JOIN core3_med_pack_inc 
+				ON CORE3_med_pack.id = core3_med_pack_inc.med_pack_id
+				WHERE core3_med_pack.id =?";
+		$stmt = $this->connect()->prepare($query);
+		$stmt->bindParam(1,$id);
+		$stmt->execute();
+		return $stmt->fetchAll();			
+	}
+
+	//View Avail Information 
+	function viewAvailInfo($id){
+		$query = "SELECT * FROM core3_med_avail 
+				INNER JOIN core3_med_pack
+				ON core3_med_avail.med_pack_id = core3_med_pack.id
+				INNER JOIN core3_med_pack_inc 
+				ON core3_med_pack.id = core3_med_pack_inc.med_pack_id
+				WHERE core3_med_avail.id = ?";
+		$stmt = $this->connect()->prepare($query);
+		$stmt->bindParam(1,$id);
+		$stmt->execute();
+		return $stmt->fetchAll();			
+	}
+	
+	//Insert Create Package
+	function createNewPackage($id,$packageName,$packagePrice,$packageDesc){
+    	$query = "INSERT INTO core3_med_pack 
+				VALUES (?,?,?,?,'Active');";
+		$stmt = $this->connect()->prepare($query);
+		$stmt->bindParam(1,$id);
+		$stmt->bindParam(2,$packageName);
+		$stmt->bindParam(3,$packagePrice);
+		$stmt->bindParam(4,$packageDesc);
+		if($stmt->execute()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	//get Id Medical Package
+	function getIdMedPackage(){
+		$query = "SELECT * FROM core3_med_pack 
+				WHERE id=(SELECT max(id) 
+				FROM core3_med_pack);";
 		$stmt = $this->connect()->prepare($query);
 		$stmt->execute();
 		return $stmt->fetchAll();
